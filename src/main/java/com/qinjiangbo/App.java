886783ee81271b0327a35a01489de8cf366bd43d
QinJiangbo <QinJiangbo@users.noreply.github.com>
2016-12-19 22:44:59 +0800

    Initial commit

---
+++ src/main/java/com/qinjiangbo/App.java
@@ -0,0 +1,11 @@
+package com.qinjiangbo;
+
+/**
+ * @date: 19/12/2016 10:44 PM
+ * @author: qinjiangbo@github.io
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, AOP");
+    }
+}
