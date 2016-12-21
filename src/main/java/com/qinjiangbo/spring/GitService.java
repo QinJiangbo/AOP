f046d1b9ba67965b08d05a25e55374032462cc1a
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-12-21 14:13:22 +0800

    add proxy services

---
+++ src/main/java/com/qinjiangbo/spring/GitService.java
@@ -0,0 +1,28 @@
+package com.qinjiangbo.spring;
+
+/**
+ * @date: 21/12/2016 2:11 PM
+ * @author: qinjiangbo@github.io
+ */
+public class GitService implements Service {
+
+    /**
+     * 拷贝某个仓库
+     *
+     * @param repository
+     */
+    @Override
+    public void fork(String repository) {
+        System.out.println("fork " + repository + " successfully!");
+    }
+
+    /**
+     * 关注某个仓库
+     *
+     * @param repository
+     */
+    @Override
+    public void star(String repository) {
+        System.out.println("star " + repository + " successfully!");
+    }
+}
---
+++ src/main/java/com/qinjiangbo/spring/Service.java
@@ -0,0 +1,12 @@
+package com.qinjiangbo.spring;
+
+/**
+ * @date: 21/12/2016 2:08 PM
+ * @author: qinjiangbo@github.io
+ */
+public interface Service {
+
+    void fork(String repository);
+
+    void star(String repository);
+}
