61784a5df5eb8c3bfaff220b599f06a43047993b
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-12-22 18:42:36 +0800

    add JDK proxy test cases

---
+++ src/test/java/com/qinjiangbo/spring/GitJdkProxyTest.java
@@ -0,0 +1,16 @@
+package com.qinjiangbo.spring;
+
+import org.junit.Test;
+
+/**
+ * @date: 22/12/2016 6:39 PM
+ * @author: qinjiangbo@github.io
+ */
+public class GitJdkProxyTest {
+
+    @Test
+    public void testFork() {
+        Service service = new GitJdkProxy(new GitService()).getProxy();
+        service.fork("tomcat");
+    }
+}
