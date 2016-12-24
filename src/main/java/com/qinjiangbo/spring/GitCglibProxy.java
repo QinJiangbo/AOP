f0626a6ea4bd74702e5f30b3a028ff4f5093b80d
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-12-24 22:13:31 +0800

    add cglib proxy

--- src/main/java/com/qinjiangbo/spring/GitCglibProxy.java
+++ src/main/java/com/qinjiangbo/spring/GitCglibProxy.java
@@ -1,8 +1,47 @@
 package com.qinjiangbo.spring;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
 /**
  * @date: 24/12/2016 10:06 PM
  * @author: qinjiangbo@github.io
  */
-public class GitCglibProxy {
+public class GitCglibProxy implements MethodInterceptor {
+
+    /**
+     * 得到代理对象
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public <T> T getProxy(Class<T> clazz) {
+        return (T) Enhancer.create(clazz, this);
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        logBefore();
+        Object result = methodProxy.invokeSuper(o, objects);
+        logAfter();
+        return result;
+    }
+
+    /**
+     * 记录方法执行前日志
+     */
+    private void logBefore() {
+        System.out.println(this.getClass().getName() + " >>> " + "before fork() invoked!");
+    }
+
+    /**
+     * 记录方法后日志
+     */
+    private void logAfter() {
+        System.out.println(this.getClass().getName() + " >>> " + "after fork() invoked!");
+    }
 }
