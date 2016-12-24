99786dbfa7042dc77d70990021d1b6a0b1f73e86
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-12-24 22:15:18 +0800

    add cglib proxy test cases

---
+++ src/test/java/com/qinjiangbo/spring/GitCglibProxyTest.java
@@ -0,0 +1,15 @@
+package com.qinjiangbo.spring;
+
+import org.junit.Test;
+
+/**
+ * @date: 24/12/2016 10:14 PM
+ * @author: qinjiangbo@github.io
+ */
+public class GitCglibProxyTest {
+
+    @Test
+    public void testFork() {
+
+    }
+}
