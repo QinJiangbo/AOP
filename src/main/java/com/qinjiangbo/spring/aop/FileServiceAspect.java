95930ea9028b7f9358c7254b67aa230193ea21d4
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-12-25 11:55:05 +0800

    add file service aspect

--- src/main/java/com/qinjiangbo/spring/aop/FileServiceAspect.java
+++ src/main/java/com/qinjiangbo/spring/aop/FileServiceAspect.java
@@ -15,6 +15,23 @@ public class FileServiceAspect {
 
     @Around("execution(* com.qinjiangbo.spring.aop.FileService.*(..))")
     public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        return null;
+        logBefore(proceedingJoinPoint);
+        Object result = proceedingJoinPoint.proceed();
+        logAfter(proceedingJoinPoint);
+        return result;
+    }
+
+    /**
+     * 记录方法执行前日志
+     */
+    private void logBefore(ProceedingJoinPoint proceedingJoinPoint) {
+        System.out.println(this.getClass().getName() + " >>> " + "before " + proceedingJoinPoint.getSignature().getName() + " invoked!");
+    }
+
+    /**
+     * 记录方法后日志
+     */
+    private void logAfter(ProceedingJoinPoint proceedingJoinPoint) {
+        System.out.println(this.getClass().getName() + " >>> " + "after " + proceedingJoinPoint.getSignature().getName() + " invoked!");
     }
 }
