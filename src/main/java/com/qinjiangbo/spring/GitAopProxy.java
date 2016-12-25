e39e0bfc8dd42cd069d289d5a79dbfa2cdf1fa87
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-12-25 10:40:01 +0800

    add aop proxy

---
+++ src/main/java/com/qinjiangbo/spring/GitAopProxy.java
@@ -0,0 +1,23 @@
+package com.qinjiangbo.spring;
+
+import org.springframework.aop.AfterReturningAdvice;
+import org.springframework.aop.MethodBeforeAdvice;
+
+import java.lang.reflect.Method;
+
+/**
+ * @date: 25/12/2016 10:33 AM
+ * @author: qinjiangbo@github.io
+ */
+public class GitAopProxy implements MethodBeforeAdvice, AfterReturningAdvice {
+
+    @Override
+    public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
+        System.out.println(this.getClass().getName() + " >>> " + "before fork() invoked!");
+    }
+
+    @Override
+    public void before(Method method, Object[] args, Object target) throws Throwable {
+        System.out.println(this.getClass().getName() + " >>> " + "after fork() invoked!");
+    }
+}
---
+++ src/test/java/com/qinjiangbo/spring/GitAopProxyTest.java
@@ -0,0 +1,20 @@
+package com.qinjiangbo.spring;
+
+import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+/**
+ * @date: 25/12/2016 10:35 AM
+ * @author: qinjiangbo@github.io
+ */
+public class GitAopProxyTest {
+
+    @Test
+    public void testFork() {
+        ProxyFactory proxyFactory = new ProxyFactory();
+        proxyFactory.setTarget(new GitService());
+        proxyFactory.addAdvice(new GitAopProxy());
+        Service service = (Service) proxyFactory.getProxy();
+        service.fork("tomcat");
+    }
+}
