1b1f6abf8c550cef1f7c3e0df7f7816b0c12f068
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-03 15:20:23 +0800

    add Utilization classes and Core aop classes

---
+++ src/main/java/com/qinjiangbo/aop/annotation/Around.java
@@ -0,0 +1,13 @@
+package com.qinjiangbo.aop.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @date: 03/01/2017 3:19 PM
+ * @author: qinjiangbo@github.io
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Around {
+}
