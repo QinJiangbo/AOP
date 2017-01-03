701a3e31f2598c3efd4f4fa6239e05c2b22b4390
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-03 11:15:41 +0800

    add annotations of aop implementation

--- src/main/java/com/qinjiangbo/aop/annotation/Before.java
+++ src/main/java/com/qinjiangbo/aop/annotation/Before.java
@@ -1,8 +1,13 @@
 package com.qinjiangbo.aop.annotation;
 
+import java.lang.annotation.*;
+
 /**
  * @date: 03/01/2017 11:12 AM
  * @author: qinjiangbo@github.io
  */
+@Target(ElementType.METHOD)
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Before {
 }
