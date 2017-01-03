75d89280391d20cebe084c10ca185d82d10e94d9
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-03 11:13:44 +0800

    add annotations of aop implementation

---
+++ src/main/java/com/qinjiangbo/aop/annotation/AspectJ.java
@@ -0,0 +1,8 @@
+package com.qinjiangbo.aop.annotation;
+
+/**
+ * @date: 03/01/2017 11:12 AM
+ * @author: qinjiangbo@github.io
+ */
+public @interface AspectJ {
+}
---
+++ src/main/java/com/qinjiangbo/aop/annotation/Bean.java
@@ -0,0 +1,8 @@
+package com.qinjiangbo.aop.annotation;
+
+/**
+ * @date: 03/01/2017 11:13 AM
+ * @author: qinjiangbo@github.io
+ */
+public @interface Bean {
+}
