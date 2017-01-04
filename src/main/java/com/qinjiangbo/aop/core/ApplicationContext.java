378d09397be0d6ce3c8f7a8b9decf2ebdaacfbec
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-04 23:32:04 +0800

    add PackageUtils class

--- src/main/java/com/qinjiangbo/aop/core/ApplicationContext.java
+++ src/main/java/com/qinjiangbo/aop/core/ApplicationContext.java
@@ -2,6 +2,8 @@ package com.qinjiangbo.aop.core;
 
 import com.qinjiangbo.aop.config.ContextConfig;
 
+import java.util.List;
+
 /**
  * @date: 04/01/2017 10:14 PM
  * @author: qinjiangbo@github.io
@@ -15,7 +17,13 @@ public class ApplicationContext {
         this.contextConfig = contextConfig;
     }
 
+    /**
+     * scan the specified packages
+     */
     private void scanPackages() {
+        List<String> packages = contextConfig.getPackages();
+        for (String package0 : packages) {
 
+        }
     }
 }
---
+++ src/main/java/com/qinjiangbo/aop/util/PackageUtils.java
@@ -0,0 +1,9 @@
+package com.qinjiangbo.aop.util;
+
+/**
+ * @date: 04/01/2017 11:12 PM
+ * @author: qinjiangbo@github.io
+ */
+public class PackageUtils {
+
+}
