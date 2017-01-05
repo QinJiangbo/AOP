ce5525f0056b9381fdd8f23ced6ba28ab8cba8a8
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-05 18:02:09 +0800

    add three advices AroundAdvice, BeforeAdvice, AfterAdvice

--- src/main/java/com/qinjiangbo/aop/advice/AroundAdvice.java
+++ src/main/java/com/qinjiangbo/aop/advice/AroundAdvice.java
@@ -1,8 +1,8 @@
-package com.qinjiangbo.aop.core;
+package com.qinjiangbo.aop.advice;
 
 /**
  * @date: 05/01/2017 6:00 PM
  * @author: qinjiangbo@github.io
  */
-public class BeforeAdvice {
+public class AroundAdvice {
 }
